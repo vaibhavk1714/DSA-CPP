7ff111e58877c932b86e80ea6113a11d209e0449
vaibhavk1714 <94043467+vaibhavk1714@users.noreply.github.com>
2023-07-13 19:23:54 +0530

    Update LinearSearch.cpp

--- LinearSearch.cpp
+++ LinearSearch.cpp
@@ -1,12 +1,12 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int linearSearch(vector<int>& arr, int n, int k) {
+int linearSearch(vector<int>& arr, int n, int key) {
 	for(int i=0;i<n;i++) {
     	if (arr[i] == key) {
       		return i; 
     	}
-  	}
+	}
 	return -1;
 }
 
