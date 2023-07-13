f608b91931a76271c7dbb6e1933f5d457d99c56a
vaibhavk1714 <94043467+vaibhavk1714@users.noreply.github.com>
2023-07-13 22:15:37 +0530

    Create BubbleSort.cpp

---
+++ BubbleSort.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void bubbleSort(vector<int>& arr, int n)
+{   
+    // Write your code here.
+    for(int i=0;i<n-1;i++) {
+        for(int j=0;j<n-i-1;j++) {
+            if(arr[j] > arr[j+1])
+                swap(arr[j], arr[j+1]);
+        }
+    }
+}
+
