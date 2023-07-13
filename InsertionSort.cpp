855d8405389e1465650ab2dc80f5ab923af88b0d
vaibhavk1714 <94043467+vaibhavk1714@users.noreply.github.com>
2023-07-13 22:50:17 +0530

    Update InsertionSort.cpp

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h> 
 using namespace std;
 
-void insertionSort(int n, vector<int> &arr){
+void insertionSort(vector<int> &arr, int n){
 	for(int i=1;i<n;i++) {
         int temp = arr[i];
         int j = i - 1;
