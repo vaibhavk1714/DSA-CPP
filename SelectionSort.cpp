be57379e0fcf177f12e97eb2cb687b77c4856ee4
vaibhavk1714 <94043467+vaibhavk1714@users.noreply.github.com>
2023-07-13 19:48:37 +0530

    Update SelectionSort.cpp

--- SelectionSort.cpp
+++ SelectionSort.cpp
@@ -14,3 +14,17 @@ void selectionSort(vector<int>& arr, int n) {
     }
 }
 
+int main() {
+	int n;
+	cin >> n;
+
+	vector<int> arr(n);
+	for(int i=0;i<n;i++) {
+		cin >> arr[i];
+	}
+
+	selectionSort(arr, n);
+	for(int i=0;i<n;i++)
+		cout << arr[i] << " "; 
+	return 0;
+}
