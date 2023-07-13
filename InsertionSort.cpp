d7fdd696f702653c5665baaf8c038a6a52529896
vaibhavk1714 <94043467+vaibhavk1714@users.noreply.github.com>
2023-07-13 22:49:06 +0530

    Update InsertionSort.cpp

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -13,3 +13,17 @@ void insertionSort(int n, vector<int> &arr){
     }
 }
 
+int main() {
+	int n;
+	cin >> n;
+
+	vector<int> arr(n);
+	for(int i=0;i<n;i++) {
+		cin >> arr[i];
+	}
+
+	insertionSort(arr, n);
+	for(int i=0;i<n;i++)
+		cout << arr[i] << " "; 
+	return 0;
+}
