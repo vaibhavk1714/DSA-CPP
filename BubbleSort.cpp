c1748d51498ddc722b12c507109e3d31dc5ca2f9
vaibhavk1714 <94043467+vaibhavk1714@users.noreply.github.com>
2023-07-13 22:16:25 +0530

    Update BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -12,3 +12,17 @@ void bubbleSort(vector<int>& arr, int n)
     }
 }
 
+int main() {
+	int n;
+	cin >> n;
+
+	vector<int> arr(n);
+	for(int i=0;i<n;i++) {
+		cin >> arr[i];
+	}
+
+	selectionSort(arr, n);
+	for(int i=0;i<n;i++)
+		cout << arr[i] << " "; 
+	return 0;
+}
